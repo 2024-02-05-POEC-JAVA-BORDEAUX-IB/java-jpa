af3654ff4ff36aca482a98755b6fbdc9fc5fe0a0
François Caremoli <francois.caremoli@wanadoo.fr>
2024-03-05 14:24:11 +0100

    Ajout méthodes utilisant SQL et JPQL

--- src/main/java/com/bigcorp/persistence/cours/BookingDaoMain.java
+++ src/main/java/com/bigcorp/persistence/cours/BookingDaoMain.java
@@ -1,5 +1,7 @@
 package com.bigcorp.persistence.cours;
 
+import java.util.List;
+
 import com.bigcorp.persistence.cours.dao.BookingDao;
 import com.bigcorp.persistence.cours.model.Booking;
 
@@ -7,11 +9,10 @@ public class BookingDaoMain {
 
 	public static void main(String[] args) {
 		BookingDao bookingDao = new BookingDao();
-		
-		
+
 		Booking newBooking = new Booking();
 		newBooking.setName("Salut, j'ai été créé via le DAO !!");
-		bookingDao.save(newBooking);
+		Booking savedBooking = bookingDao.save(newBooking);
 
 		Booking b = bookingDao.findById(4l);
 
@@ -19,6 +20,14 @@ public class BookingDaoMain {
 			System.out.println("Pas de booking trouvé");
 		} else {
 			System.out.println("Pas de booking trouvé");
+		}
+
+		String bookingName = bookingDao.getNameWithNativeSQL(savedBooking.getId());
+		System.out.println("Avec SELECT en vrai SQL , j'ai récupéré " + bookingName);
+
+		List<Booking> bookingFromName = bookingDao.getBookingFromName("Salut, j'ai été créé via le DAO !!");
+		for (Booking booking : bookingFromName) {
+			System.out.println("J'ai trouvé le livre : " + booking.getName());
 		}
 
 	}
--- src/main/java/com/bigcorp/persistence/cours/dao/BookingDao.java
+++ src/main/java/com/bigcorp/persistence/cours/dao/BookingDao.java
@@ -1,10 +1,15 @@
 package com.bigcorp.persistence.cours.dao;
 
+import java.util.List;
+import java.util.Objects;
+
 import com.bigcorp.persistence.cours.model.Booking;
 import com.bigcorp.persistence.cours.util.PersistenceFactory;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 
 public class BookingDao {
 
@@ -42,5 +47,30 @@ public class BookingDao {
 			entityManager.close();
 		}
 	}
+
+	public String getNameWithNativeSQL(Long id) {
+		EntityManager em = PersistenceFactory.INSTANCE.getEntityManager();
+		Query sqlQuery = em.createNativeQuery("SELECT NAME FROM BOOKING WHERE ID = " + id);
+		List<?> result = sqlQuery.getResultList();
+		if (result.isEmpty()) {
+			return null;
+		}
+		// else
+		return Objects.toString(result.get(0));
+	}
+
+	public List<Booking> getBookingFromName(String name) {
+		EntityManager em = PersistenceFactory.INSTANCE.getEntityManager();
+		TypedQuery<Booking> query = em.createQuery(
+				"select b "
+				+ " from Booking b "
+				+ " where b.name = :paramName ", Booking.class);
+		query.setParameter("paramName", name);
+		List<Booking> resultList = query.getResultList();
+		em.close();
+		return resultList;
+	}
+	
+	
 
 }
